c5f0a8dadf178e1ed6ad3bc40ff4e3057b0e51f2
Brandon Buchholz <blue.buchholz@gmail.com>
2018-01-13 18:27:00 -0800

    set routes for getting all books and fetching 1 book, switched drivers

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const pg = require('pg')
 const cors = require('cors')
 const bodyParser = require('body-parser').urlencoded({extended: true});
 
-const PORT = process.env.PORT || 3000
-const DATABASE_URL = process.env.DATABASE_URL || 'postgres://localhost:5432/books_app'
+const PORT = 3004
+const DATABASE_URL = 'postgres://localhost:5432/books_app'
 
 const client = new pg.Client(DATABASE_URL)
 client.connect()
@@ -24,12 +24,11 @@ app.get('/api/v1/books', (req, res) => {
         .catch(err => console.error(err))
 })
 app.get('/api/v1/books/:id', (req, res) => {
-    console.log('Hit api books route')
 	client.query(`
 	SELECT * FROM books WHERE book_id=${req.params.id};
 	`).then(result => res.send(result.rows[0]))
 })
-app.post('/api/v1/books',express.JSON(), express.urlencoded({extended:true}), (req, res) => {
+app.post('/api/v1/books', express.urlencoded({extended:true}), (req, res) => {
     console.log(req.body)
 	client.query(`
 	INSERT INTO books 
